9227219c107d3223a6d5fd69cce4d4e0cde178c6
alpaca-tc <alprhcp666@gmail.com>
2014-02-05 00:19:24 +0900

    Append todo to strip_comment.rb

--- lib/strip_comment.rb
+++ lib/strip_comment.rb
@@ -1,4 +1,9 @@
 require 'strip_comment/version'
 
 module StripComment
+  # [todo] - Encodes file by the correct encoding
+  # [todo] - Defines scanner for each files
+  # [todo] - Parser delegates to scanner to scan file
+  # [todo] - Initializes code_object for comment, code and as so on
+  # [todo] - Defines method to parses file
 end
