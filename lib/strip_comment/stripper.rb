d45be2a310339ede2f125368e385fb3a3717e327
alpaca-tc <alpaca-tc@alpaca.tc>
2014-02-07 20:52:03 +0900

    Filters find directories by configuration

--- lib/strip_comment/stripper.rb
+++ lib/strip_comment/stripper.rb
@@ -7,11 +7,11 @@ class StripComment::Stripper
   end
 
   def list_up_files
-    # [todo] - files, directriesの実装
-    root_path = configuration.root_path
-    root_path = File.dirname(root_path) unless File.directory?(root_path)
-
-    Dir["#{root_path}/**/*"].select { |v| enabled_file?(v) }
+    @files ||= begin
+      root_path = configuration.root_path
+      root_path = File.dirname(root_path) unless File.directory?(root_path)
+      Dir["#{root_path}/**/*"]
+    end.select { |v| enabled_file?(v) }
   end
 
   def comments
@@ -25,12 +25,44 @@ class StripComment::Stripper
   private
 
   def enabled_file?(file_path)
-    return false if File.directory?(file_path)
+    # [review] - Complex branch function...
+    if File.directory?(file_path) || ignore_file?(file_path)
+      false
+    elsif configuration.directories.empty? && configuration.files.empty?
+      true
+    elsif !configuration.directories.empty? && configuration.files.empty?
+      directories_contained?(file_path)
+    elsif configuration.directories.empty? && !configuration.files.empty?
+      files_contained?(file_path)
+    else
+      directories_contained?(file_path) && files_contained?(file_path)
+    end
+  end
 
-    configuration.ignore_list.each do |regexp|
-      return false if regexp =~ file_path
+  def files_contained?(file_path)
+    # [todo] - Checking files at most one
+    configuration.files.each do |pattern|
+      return true if Regexp.new(pattern) =~ file_path
     end
 
-    true
+    false
+  end
+
+  def directories_contained?(file_path)
+    # [todo] - Checking directories at most one
+    configuration.directories.each do |pattern|
+      return true if Regexp.new(pattern) =~ file_path
+    end
+
+    false
+  end
+
+  def ignore_file?(file_path)
+    # [todo] - Checking ignore_list at most one
+    configuration.ignore_list.each do |pattern|
+      return true if Regexp.new(pattern) =~ file_path
+    end
+
+    false
   end
 end
--- spec/strip_comment/stripper_spec.rb
+++ spec/strip_comment/stripper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module StripComment
   describe Stripper do
     let(:options) { { root_path: root_path } }
-    let(:root_path) { File.dirname(__FILE__) }
+    let(:root_path) { File.expand_path('../../assets/stripper', __FILE__) }
     let(:stripper) { Stripper.new(options) }
 
     describe 'ClassMethods' do
@@ -52,6 +52,16 @@ module StripComment
         it 'returns array contains list of files' do
           expect(subject).to eql expected
         end
+
+        context 'when configuration defines files' do
+          let(:options) { { root_path: root_path, files: files } }
+          let(:files) { %w[test.c test.rb] }
+          let(:expected) { files.map { |v| "#{root_path}/#{v}" } }
+
+          it 'returns only files defined' do
+            expect(subject).to eql expected
+          end
+        end
       end
 
       describe '#comments' do
@@ -86,6 +96,61 @@ module StripComment
           let(:file_path) { File.expand_path('..', __FILE__) }
 
           it { should be_false }
+        end
+      end
+
+      describe '#ignore_file?' do
+        let(:options) { { root_path: root_path, ignore_list: ignore_list } }
+        subject { stripper.send(:ignore_file?, file_path) }
+
+        context 'when ignore_list contains file_path' do
+          let(:ignore_list) { %w!.*\.rb! }
+          let(:file_path) { 'bar.rb' }
+          it { should be_true }
+        end
+
+        context 'when ignore_list does not contain file_path' do
+          let(:ignore_list) { [] }
+          let(:file_path) { 'bar.rb' }
+          it { should be_false }
+        end
+      end
+
+      describe '#files_contained?' do
+        let(:options) { { root_path: root_path, files: files } }
+        subject { stripper.send(:files_contained?, file_path) }
+
+        context 'when setting of directories sets to [.*\.rb]' do
+          let(:files) { %w!.*\.rb! }
+
+          context 'given "assets/bar.c"' do
+            let(:file_path) { File.expand_path('../assets/bar.c', __FILE__) }
+            it { should be_false }
+          end
+
+          context 'given "assets/bar.rb"' do
+            let(:file_path) { File.expand_path('../assets/children/bar.rb', __FILE__) }
+            it { should be_true }
+          end
+        end
+      end
+
+      describe '#directories_contained?' do
+        let(:options) { { root_path: root_path, directories: directories } }
+        subject { stripper.send(:directories_contained?, file_path) }
+
+        context 'when setting of directories sets to [assets/children]' do
+          let(:directories) { %w!assets/children! }
+
+          context 'given "assets/bar.rb"' do
+            let(:file_path) { File.expand_path('../assets/bar.rb', __FILE__) }
+            it { should be_false }
+          end
+
+          context 'given "assets/children/bar.rb"' do
+            let(:file_path) { File.expand_path('../assets/children/bar.rb', __FILE__) }
+            it { should be_true }
+          end
         end
       end
     end
