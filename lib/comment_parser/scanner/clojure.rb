0b98b5a81fb65d773d14533db21112290b1f43d5
alpaca-tc <alpaca-tc@alpaca.tc>
2014-02-12 10:05:28 +0900

    Imploves scanner of clojure

--- lib/comment_parser/scanner/clojure.rb
+++ lib/comment_parser/scanner/clojure.rb
@@ -1,34 +1,9 @@
 class CommentParser::Scanner::Clojure < CommentParser::Scanner
-  disable!
+  include CommentParser::Scanner::Concerns::SimpleScanner
+
   filename /\.clj$/
   filetype 'clojure'
 
-  def scan
-    scanner = build_scanner
-
-    until scanner.eos?
-      case
-      when scanner.scan(/"/)
-        scanner.scan(/.*?(?<!\\)"/m)
-      when scanner.scan(/'/)
-        scanner.scan(/.*?(?<!\\)'/m)
-      when scanner.scan(%r!;+!)
-        identify_single_comment
-      when scanner.scan(/(\w|\W)/)
-        next
-      when scanner.scan(CommentParser::Scanner::REGEXP[:BREAK])
-        next
-      else
-        raise_report
-      end
-    end
-  end
-
-  private
-
-  def identify_single_comment
-    line_no = current_line
-    line = build_scanner.scan(/.*$/).strip
-    add_comment(line_no, line, { type: :singleline })
-  end
+  define_default_bracket
+  define_rule open: /;+/
 end
