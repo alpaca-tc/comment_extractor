6183a20246d3a15d0241770bc41a505d9079807d
alpaca-tc <alpaca-tc@alpaca.tc>
2014-02-12 11:21:02 +0900

    Adds test to scanner of scala

--- spec/comment_parser/scanner/scala_spec.rb
+++ spec/comment_parser/scanner/scala_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 class CommentParser::Scanner
   describe Scala do
-    subject { Scala }
-    it { should include Concerns::SlashScanner }
+    it_behaves_like 'scanning source code'
   end
 end
