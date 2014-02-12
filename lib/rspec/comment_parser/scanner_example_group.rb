8aa2a2071303006e660a1ea3c6f10f23f2ee10da
alpaca-tc <alpaca-tc@alpaca.tc>
2014-02-12 14:03:49 +0900

    Removes active_support

--- lib/rspec/comment_parser/scanner_example_group.rb
+++ lib/rspec/comment_parser/scanner_example_group.rb
@@ -1,12 +1,8 @@
-require 'active_support/concern'
-require 'active_support/core_ext/string'
-
 RSpec.configure do |config|
   config.add_setting :source_code_path, default: 'spec/assets/source_code'
 end
 
 module RSpec::CommentParser::ScannerExampleGroup
-  extend ActiveSupport::Concern
   include RSpec::CommentParser::Matchers::DetectComment
 
   module ClassMethods
@@ -25,61 +21,72 @@ module RSpec::CommentParser::ScannerExampleGroup
     file_name ? "#{dir}/#{file_name}" : dir
   end
 
-  included do
-    metadata[:type] = :scanner
+  def default_file_name
+    described_class.to_s[/[^:]+$/].gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
+  end
 
-    # Skips test when scanner has already disabled
-    if metadata[:disabled] == true
-      shared_examples_for 'disabled' do
-        subject { described_class.disabled? }
-        it { expect(subject).to be_truthy }
+  def self.included(k)
+    k.class_eval do
+      extend ClassMethods
+      metadata[:type] = :scanner
+
+      # Skips test when scanner has already disabled
+      if metadata[:disabled] == true
+        shared_examples_for 'disabled' do
+          subject { described_class.disabled? }
+          it { expect(subject).to be_truthy }
+        end
+
+        it_behaves_like 'disabled'
+
+        pending 'skipping test for scanner'
+
+        next
       end
 
-      it_behaves_like 'disabled'
+      subject { scanner }
 
-      pending 'skipping test for scanner'
+      let(:file_path) { default_file_name }
+      let(:source_code) { CommentParser::File.new(source_code_path(file_path)) }
+      let(:scanner) { build_scanner(source_code) }
 
-      next
-    end
+      context '.new' do
+        it { expect { build_scanner(source_code) }.to_not raise_error }
+      end
 
-    subject { scanner }
-
-    let(:file_path) { described_class.to_s[/[^:]+$/].underscore }
-    let(:source_code) { CommentParser::File.new(source_code_path(file_path)) }
-    let(:scanner) { build_scanner(source_code) }
-
-    context '.new' do
-      it { expect { build_scanner(source_code) }.to_not raise_error }
-    end
-
-    shared_examples_for 'scanning source code' do |file_name|
-      context '.scan' do
-        let(:comments) do
-          source_code.content.split("\n").each_with_object({}) do |line, memo|
-            if %r!(?<comment>\[-(?<line_number>\d+)-\].*)$! =~ line
-              comment.sub!(/(?<=\[-end-\]).*$/, '') if comment =~ /\[-end-\]/
-              memo[line_number.to_i] = comment
+      shared_examples_for 'scanning source code' do |file_name|
+        context '.scan' do
+          let(:comments) do
+            source_code.content.split("\n").each_with_object({}) do |line, memo|
+              if %r!(?<comment>\[-(?<line_number>\d+)-\].*)$! =~ line
+                comment.sub!(/(?<=\[-end-\]).*$/, '') if comment =~ /\[-end-\]/
+                memo[line_number.to_i] = comment
+              end
             end
           end
-        end
 
-        before do
-          scanner.scan
-        end
+          before do
+            scanner.scan
+          end
 
-        it 'scans source_code' do
-          expect { scanner.comments }.to detect_comment(comments)
+          it 'scans source_code' do
+            expect { scanner.comments }.to detect_comment(comments)
+          end
         end
       end
-    end
 
-    shared_examples_for 'detecting shebang' do |*shebangs|
-      # let dummy source code
-      let(:source_code) { CommentParser::File.new(__FILE__) }
+      shared_examples_for 'detecting shebang' do |*shebangs|
+        # let dummy source code
+        let(:source_code) { CommentParser::File.new(__FILE__) }
 
-      shebangs.each do |shebang|
-        it 'detects shebang' do
-          expect(shebang).to match scanner.shebang
+        shebangs.each do |shebang|
+          it 'detects shebang' do
+            expect(shebang).to match scanner.shebang
+          end
         end
       end
     end
