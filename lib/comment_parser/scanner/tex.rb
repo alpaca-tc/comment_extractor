414a1d0d24672ee1f1bc4daed717b6a6c42d87c8
alpaca-tc <alpaca-tc@alpaca.tc>
2014-02-12 11:51:31 +0900

    Implements scanner of tex

--- lib/comment_parser/scanner/tex.rb
+++ lib/comment_parser/scanner/tex.rb
@@ -1,5 +1,10 @@
 class CommentParser::Scanner::Tex < CommentParser::Scanner
-  disable!
+  include CommentParser::Scanner::Concerns::SimpleScanner
+
   filename /\.tex$/
   filetype 'tex'
+
+  define_bracket '{'
+  define_rule open: /(?<!\\)%/
+  define_ignore_patterns /\\%/
 end
