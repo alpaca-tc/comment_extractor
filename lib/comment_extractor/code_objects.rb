d426826099462ec18f934152795e9bafc4d6f02c
alpaca-tc <alpaca-tc@alpaca.tc>
2014-02-17 20:52:06 +0900

    Refactor code_objects

--- lib/comment_extractor/code_objects.rb
+++ lib/comment_extractor/code_objects.rb
@@ -1,27 +1,45 @@
-require 'forwardable'
+require 'delegate'
 
 module CommentExtractor
-  class CodeObjects
-    extend Forwardable
-
+  class CodeObjects < DelegateClass(Array)
     attr_accessor :file
 
     def initialize(file: nil)
       @file = file
-      @code_objects = []
+      super([])
     end
 
     def <<(code_object)
+      super(initialize_code_object(code_object))
+    end
+
+    def push(*code_object_array)
+      arguments = code_object_array.map { |v| initialize_code_object(v) }
+      super(arguments)
+    end
+
+    def concat(*code_object_arrays)
+      arguments = code_object_arrays.flatten.map { |v| initialize_code_object(v) }
+      super(arguments)
+    end
+
+    def inspect
+      attributes = instance_variables.map { |v| "@#{v}=#{instance_variable_get(v)}" }
+      attributes = attributes.empty? ? '' : " #{attributes.join(', ')}"
+      object_id = '0x%x' % (self.object_id << 1)
+      "#<#{self.class}:#{object_id}#{attributes}>"
+    end
+    alias :to_s :inspect
+
+    private
+
+    def initialize_code_object(code_object)
       unless code_object.is_a?(CodeObject)
         message = "no implicit conversion of #{code_object.class} into CodeObject"
         raise TypeError, message
       end
 
       code_object.metadata[:parent] = self
-
-      @code_objects << code_object
     end
-
-    def_delegators :@code_objects, :[], :size, :length, :each, :map, :each_with_index, :each_with_object, :map!
   end
 end
--- spec/comment_extractor/code_objects_spec.rb
+++ spec/comment_extractor/code_objects_spec.rb
@@ -4,12 +4,13 @@ require 'comment_extractor/code_objects'
 
 module CommentExtractor
   describe CodeObjects do
+    let(:file_path) { 'path/to/file' }
+
     describe 'ClassMethods' do
       describe '.new' do
         subject { described_class.new(file: file_path) }
-        let(:file_path) { 'path/to/file' }
 
-        it 'returns a instance of CodeObjects' do
+        it 'initializes a instance of CodeObjects' do
           expect(subject).to be_an_instance_of CodeObjects
           expect(subject.file).to eql file_path
         end
@@ -17,28 +18,40 @@ module CommentExtractor
     end
 
     describe 'InstanceMethods' do
-      let(:code_objects) { described_class.new(file: 'path/to/file') }
+      let(:described_instance) { described_class.new(file: file_path) }
+      let(:code_object) { CodeObject.new(value: 3) }
 
-      describe '#<<' do
-        subject { code_objects << code_object }
-
-        context 'given a not instance of CodeObject::Comment' do
+      shared_context 'appending code_object to self' do
+        context 'given not a instance of CodeObject::Comment' do
           let(:code_object) { nil }
           it { expect { subject }.to raise_error(TypeError) }
         end
 
         context 'given instance of CodeObject::Comment' do
-          let(:code_object) { CodeObject.new(value: 3) }
-
           it 'adds a information of self to argument' do
             subject
-            expect(code_object.metadata[:parent]).to eql code_objects
+            expect(code_object.metadata[:parent]).to equal described_instance
           end
 
           it 'adds code_object' do
-            expect { subject }.to change { code_objects.size }.to(1).from(0)
+            expect { subject }.to change { described_instance.size }.to(1).from(0)
           end
         end
+      end
+
+      describe '#<<' do
+        subject { described_instance << code_object }
+        it_behaves_like 'appending code_object to self'
+      end
+
+      describe '#push' do
+        subject { described_instance.push code_object, code_object }
+        it_behaves_like 'appending code_object to self'
+      end
+
+      describe '#concat' do
+        subject { described_instance.concat [code_object] }
+        it_behaves_like 'appending code_object to self'
       end
     end
   end
